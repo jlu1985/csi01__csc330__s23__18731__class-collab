4c48ad5f72525b9f0067e6b3e78237d2eef457f1
Jia Lu <jlu1985@gmail.com>
2023-03-14 20:19:11 -0400

    make sizes

--- week8-abstraction/src/main/java/edu/cuny/csi/s23/csc330/pizzeria/MenuLoader.java
+++ week8-abstraction/src/main/java/edu/cuny/csi/s23/csc330/pizzeria/MenuLoader.java
@@ -3,9 +3,24 @@ package edu.cuny.csi.s23.csc330.pizzeria;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class MenuLoader {
+
+    private static PizzaMenuItem buildPizzaMenu(Properties p, int i) {
+        String property = p.getProperty(String.format("menu.pizza.%d.type", i));
+        PizzaType pizzaType = PizzaType.valueOf(property.trim().toUpperCase());
+
+        PizzaMenuItem pizzaMenuItem = new PizzaMenuItem();
+        pizzaMenuItem.setPizzaType(pizzaType);
+        Arrays.stream(p.getProperty(String.format("menu.pizza.%d.size", i)).split(","))
+                .map(Integer::parseInt)
+                .forEach(pizzaMenuItem::addSize);
+
+        //    System.out.println(pizzaMenuItem);
+        return pizzaMenuItem;
+    }
 
     public Menu createMenu() throws IOException, URISyntaxException {
         Properties p = new Properties();
@@ -29,18 +44,5 @@ public class MenuLoader {
             menu.add(buildPizzaMenu(p, i));
         }
         return menu;
-    }
-
-    private static PizzaMenuItem buildPizzaMenu(Properties p, int i) {
-        String property = p.getProperty(String.format("menu.pizza.%d.type", i));
-        PizzaType pizzaType = PizzaType.valueOf(property.trim().toUpperCase());
-
-        String sizeStr = p.getProperty(String.format("menu.pizza.%d.size", i));
-        int size = Integer.parseInt(sizeStr);
-        PizzaMenuItem pizzaMenuItem = new PizzaMenuItem();
-        pizzaMenuItem.setPizzaType(pizzaType);
-        pizzaMenuItem.addSize(size);
-        //    System.out.println(pizzaMenuItem);
-        return pizzaMenuItem;
     }
 }
